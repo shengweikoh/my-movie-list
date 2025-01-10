4483919ede2f4fb161332a21562ceae39aeeb5dc
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2025-01-10 14:05:40 +0800

    removed redundant import from firebaseconfig

--- frontend/src/firebase/FirebaseConfig.js
+++ frontend/src/firebase/FirebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Firebase configuration from environment variables
 const firebaseConfig = {
